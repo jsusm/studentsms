0a20ffdb247b40074bcce805e3f5d9120489ddab
jsus marcano <jesusjaviermarcanomora.2005@gmail.com>
2023-01-25 11:50:18 -0400

    load routes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 import morgan from 'morgan'
 import * as config from './config'
+import router from './routes'
 
 const app = express()
 if(config.env === 'development'){
   app.use(morgan('dev'))
 }
+
+app.use(router)
 
 app.listen(config.PORT, () => {
   console.log(`Express app listen on port: ${config.PORT}`)
