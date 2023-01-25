017d24b060e71356a623030a92a0f33dea784b07
jsus marcano <jesusjaviermarcanomora.2005@gmail.com>
2023-01-25 09:47:59 -0400

    pull config information to another file

---
+++ src/config.ts
@@ -0,0 +1,2 @@
+export const PORT = process.env.PORT || 8080
+export const env = process.env.NODE_ENV || 'development'
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,12 @@
 import express from 'express'
 import morgan from 'morgan'
+import * as config from './config'
 
 const app = express()
-const env = process.env.NODE_ENV || 'development'
-if(env === 'development'){
+if(config.env === 'development'){
   app.use(morgan('dev'))
 }
 
-const PORT = process.env.PORT || 8080
-
-app.listen(PORT, () => {
-  console.log(`Express app listen on port: ${PORT}`)
+app.listen(config.PORT, () => {
+  console.log(`Express app listen on port: ${config.PORT}`)
 })
