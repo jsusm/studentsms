03f4b45c3fd0ebe272ce3934077b1b142d889467
jsus marcano <jesusjaviermarcanomora.2005@gmail.com>
2023-01-27 10:32:15 -0400

    Create Student model class

---
+++ src/students/student.model.ts
@@ -0,0 +1,9 @@
+export class Student {
+  constructor(
+    public id: number,
+    public name: string,
+    public lastName: string,
+    public bornDate: Date,
+    public description: string,
+  ){ }
+}
