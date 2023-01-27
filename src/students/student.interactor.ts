188ca3152ad30404c76baca016269b0b6b325392
jsus marcano <jesusjaviermarcanomora.2005@gmail.com>
2023-01-27 10:42:06 -0400

    Fix repository types, create student interactor

---
+++ src/students/student.interactor.ts
@@ -0,0 +1,23 @@
+import { CreateStudentData, StudentRepository, UpdateStudentData } from './student.repository'
+
+export class StudentInteractor {
+  repository: StudentRepository
+  constructor(repository: StudentRepository){
+    this.repository = repository
+  }
+  async create(data: CreateStudentData){
+    return await this.repository.create(data)
+  }
+  async update(criteria: {id: number}, data: UpdateStudentData){
+    return await this.repository.update(criteria, data)
+  }
+  async delete(criteria: {id: number}){
+    return await this.repository.delete(criteria)
+  }
+  async read(){
+    return await this.repository.read()
+  }
+  async readOne(criteria: { id: number }){
+    return await this.repository.readOne(criteria)
+  }
+}
--- src/students/student.repository.ts
+++ src/students/student.repository.ts
@@ -1,8 +1,11 @@
 import { Student } from './student.model'
 
+export type CreateStudentData = Omit<typeof Student, 'id'>
+export type UpdateStudentData = Partial<CreateStudentData>
+
 export interface StudentRepository {
-  create(data: { title: string, pph: number }): Promise<Student>
-  update(criteria: { id: number }, data: { title?: string, pph?: number }): Promise<Student>
+  create(data: CreateStudentData): Promise<Student>
+  update(criteria: { id: number }, data: UpdateStudentData): Promise<Student>
   delete(criteria: { id: number }): Promise<Student>
   readOne(criteria: { id: number }): Promise<Student | null>
   read(): Promise<Array<Student>>
