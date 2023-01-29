8aa981af407c70d275d5b3d9f9d26d91004d03da
jsus marcano <jesusjaviermarcanomora.2005@gmail.com>
2023-01-28 20:56:33 -0400

    register sesssion controller

--- src/controllers.ts
+++ src/controllers.ts
@@ -5,10 +5,14 @@ import { KlassPrismaRepository } from './klass/klass.prisma'
 import { StudentController } from './students/student.controller'
 import { StudentInteractor } from './students/student.interactor'
 import { StudentPrismaRepository } from './students/student.prisma'
+import { SessionController } from './sessions/session.controller'
+import { SessionPrismaRepository } from './sessions/session.prisma'
+import { SessionInteractor } from './sessions/session.interactor'
 
 const controllers = {
   'classes': new KlassController(new KlassInteractor(new KlassPrismaRepository())),
   'students': new StudentController(new StudentInteractor(new StudentPrismaRepository())),
+  'sessions': new SessionController(new SessionInteractor(new SessionPrismaRepository()))
 }
 
 export const controllerRouter = controlerLoader(controllers)
