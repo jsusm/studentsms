7c08d8153edba4b173cdba66c39ce6bb633024af
jsus marcano <jesusjaviermarcanomora.2005@gmail.com>
2023-01-30 10:26:43 -0400

    Add restriction to update an attended session

--- src/sessions/session.interactor.ts
+++ src/sessions/session.interactor.ts
@@ -1,3 +1,5 @@
+import { InteractorError, InteractorResult } from "../lib/interactor/interactorResult";
+import { Session } from "./session.model";
 import { CreateSessionData, SessionRepository, UpdateSessionData } from "./session.repository";
 
 export class SessionInteractor {
@@ -18,9 +20,25 @@ export class SessionInteractor {
     const session = await this.repository.create(data)
     return session
   }
-  async update(criteria: {id: number}, data: UpdateSessionData) {
-    const session = await this.repository.update(criteria, data)
-    return session
+  async update(criteria: {id: number}, data: UpdateSessionData): Promise<InteractorResult<Session>> {
+    const session = await this.repository.readOne(criteria)
+    if(!session) {
+      return {
+        success: false,
+        error: new InteractorError(`Session with id ${criteria.id} does not exits.`, "notFound")
+      }
+    }
+    if(session.attended) {
+      return {
+        success: false,
+        error: new InteractorError(`Cannot update an attended session.`, "notAllowed")
+      }
+    }
+    const updatedSession = await this.repository.update(criteria, data)
+    return {
+      success: true,
+      data: updatedSession,
+    }
   }
   async delete(criteria: {id: number}){
     await this.repository.delete(criteria)
