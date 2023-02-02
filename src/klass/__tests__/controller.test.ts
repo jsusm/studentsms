0fec43ef312cdeab108e0b80ddbc0e5832ab8247
jsus marcano <jesusjaviermarcanomora.2005@gmail.com>
2023-02-02 11:28:44 -0400

    use new app config, for klass.controller's tests

--- src/klass/__tests__/controller.test.ts
+++ src/klass/__tests__/controller.test.ts
@@ -1,21 +1,12 @@
 import express from 'express'
 import request from 'supertest';
-import { controlerLoader } from '../../lib/controller/controllerLoader';
 import { KlassController } from "../klass.controller";
 import { KlassMockRepository, klassMockData } from "../mock.repository";
-import { requestErrorHandler, requestZodErrorHander } from '../../errors'
 import supertest from 'supertest';
+import { setUpApp } from '../../app';
 
 let app: express.Application
 
-function setUpController() {
-  app = express()
-  app.use(express.json())
-  app.use('/', controlerLoader({ 'classes': new KlassController(new KlassMockRepository()) }))
-  app.use(requestZodErrorHander)
-  app.use(requestErrorHandler)
-  return app
-}
 function jsonReq(t: supertest.Test) {
   return t
     .set('Content-Type', 'application/json')
@@ -23,45 +14,45 @@ function jsonReq(t: supertest.Test) {
 }
 
 beforeEach(() => {
-  app = setUpController()
+  app = setUpApp({'classes': new KlassController(new KlassMockRepository)})
 })
 
 describe('Klass controller', () => {
-  describe('GET /classes', () => {
+  describe('GET /api/v2/classes', () => {
     it('Should return a list of klasses', async () => {
-      const res = await request(app).get('/classes')
+      const res = await request(app).get('/api/v2/classes')
       expect(res.status).toBe(200)
       expect(res.body).toStrictEqual(klassMockData)
     })
   })
-  describe('GET /classes/:id', () => {
+  describe('GET /api/v2/classes/:id', () => {
     describe("Given an id", () => {
       it("Should return a klass", async () => {
-        const res = await request(app).get('/classes/1').send()
+        const res = await request(app).get('/api/v2/classes/1').send()
         expect(res.status).toBe(200)
         expect(res.body).toHaveProperty('klass')
       })
       it("Should return a klass' sessions", async () => {
-        const res = await request(app).get('/classes/1').send()
+        const res = await request(app).get('/api/v2/classes/1').send()
         expect(res.status).toBe(200)
         expect(res.body).toHaveProperty('sessions')
       })
     })
     describe("Given an non-existent id", () => {
       it("Should return a 404", async () => {
-        const res = await request(app).get('/classes/1000').send()
+        const res = await request(app).get('/api/v2/classes/1000').send()
         expect(res.status).toBe(404)
       })
     })
   })
-  describe('POST /classes', () => {
+  describe('POST /api/v2/classes', () => {
     describe("Given a valid payload", () => {
       it("Should return a 200", async () => {
         const payload = {
           title: 'Test class',
           pph: 2,
         }
-        const res = await jsonReq(request(app).post('/classes'))
+        const res = await jsonReq(request(app).post('/api/v2/classes'))
           .send(payload)
         expect(res.status).toBe(201)
       })
@@ -70,7 +61,7 @@ describe('Klass controller', () => {
       it("Should return a 400", async () => {
         const payload = {
         }
-        const res = await jsonReq(request(app).post('/classes'))
+        const res = await jsonReq(request(app).post('/api/v2/classes'))
           .send(payload)
         expect(res.status).toBe(400)
         expect(res.body).toMatch(/pph/)
@@ -78,23 +69,23 @@ describe('Klass controller', () => {
       })
     })
   })
-  describe('PATCH /classes/:id', () => {
+  describe('PATCH /api/v2/classes/:id', () => {
     describe("Given a valid payload and a existent id", () => {
       it("Should return a 200", async () => {
         const payload = {
           title: 'Test class',
           pph: 2,
         }
-        const res = await jsonReq(request(app).patch('/classes/1')).send(payload)
+        const res = await jsonReq(request(app).patch('/api/v2/classes/1')).send(payload)
         expect(res.status).toBe(200)
         expect(res.body).toMatchObject(payload)
       })
     })
   })
-  describe('DELETE /classes/:id', () => {
+  describe('DELETE /api/v2/classes/:id', () => {
     describe("Given a valid payload and a existent id", () => {
       it("Should return a 200", async () => {
-        const res = await jsonReq(request(app).delete('/classes/1'))
+        const res = await jsonReq(request(app).delete('/api/v2/classes/1'))
         expect(res.status).toBe(200)
       })
     })
