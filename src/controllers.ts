443d5f79b728e8add2bd014d9a3f9d482033e40d
jsus marcano <jesusjaviermarcanomora.2005@gmail.com>
2023-02-05 12:05:57 -0400

    register payment controller to controllerConfig

--- src/controllers.ts
+++ src/controllers.ts
@@ -4,9 +4,12 @@ import { StudentController } from './students/student.controller'
 import { StudentPrismaRepository } from './students/student.prisma'
 import { SessionController } from './sessions/session.controller'
 import { SessionPrismaRepository } from './sessions/session.prisma'
+import { PaymentController } from './payments/payment.controller'
+import { PaymentPrismaRepository } from './payments/payment.prisma'
 
 export const controllerConfig = {
   'classes': new KlassController(new KlassPrismaRepository()),
   'students': new StudentController(new StudentPrismaRepository()),
   'sessions': new SessionController(new SessionPrismaRepository()),
+  'payments': new PaymentController(new PaymentPrismaRepository()),
 }
